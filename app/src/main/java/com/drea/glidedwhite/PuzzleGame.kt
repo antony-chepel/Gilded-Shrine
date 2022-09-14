960f14d94e0a2124c65436150eb789293de87b95
antony-chepel <72796562+antony-chepel@users.noreply.github.com>
2022-09-14 08:40:45 +0300

    Update PuzzleGame.kt

--- app/src/main/java/com/drea/glidedwhite/PuzzleGame.kt
+++ app/src/main/java/com/drea/glidedwhite/PuzzleGame.kt
@@ -28,9 +28,7 @@ class PuzzleGame : AppCompatActivity() {
         val assetName = intent.getStringExtra("assetName")
         mCurrentPhotoPath = intent.getStringExtra("mCurrentPhotoPath")
         mCurrentPhotoUri = intent.getStringExtra("mCurrentPhotoUri")
-
-        // run image related code after the view was laid out
-        // to have all dimensions calculated
+        
         imageView.post {
             if (assetName != null) {
                 setPicFromAsset(assetName, imageView)
@@ -41,7 +39,7 @@ class PuzzleGame : AppCompatActivity() {
             }
             pieces = splitImage()
             val touchListener = TouchListener(this@PuzzleGame)
-            // shuffle pieces order
+          
             Collections.shuffle(pieces)
             for (piece in pieces!!) {
                 piece!!.setOnTouchListener(touchListener)
@@ -56,24 +54,23 @@ class PuzzleGame : AppCompatActivity() {
     }
 
     private fun setPicFromAsset(assetName: String, imageView: ImageView) {
-        // Get the dimensions of the View
         val targetW = imageView.width
         val targetH = imageView.height
         val am = assets
         try {
             val `is` = am.open("img/$assetName")
-            // Get the dimensions of the bitmap
+           
             val bmOptions = BitmapFactory.Options()
             bmOptions.inJustDecodeBounds = true
             BitmapFactory.decodeStream(`is`, Rect(-1, -1, -1, -1), bmOptions)
             val photoW = bmOptions.outWidth
             val photoH = bmOptions.outHeight
 
-            // Determine how much to scale down the image
+           
             val scaleFactor = Math.min(photoW / targetW, photoH / targetH)
             `is`.reset()
 
-            // Decode the image file into a Bitmap sized to fill the View
+            
             bmOptions.inJustDecodeBounds = false
             bmOptions.inSampleSize = scaleFactor
             bmOptions.inPurgeable = true
@@ -92,7 +89,7 @@ class PuzzleGame : AppCompatActivity() {
         val imageView = findViewById<ImageView>(R.id.imageView)
         val pieces = ArrayList<PuzzlePiece?>(piecesNumber)
 
-        // Get the scaled bitmap of the source image
+        
         val drawable = imageView.drawable as BitmapDrawable
         val bitmap = drawable.bitmap
         val dimensions = getBitmapPositionInsideImageView(imageView)
@@ -112,11 +109,11 @@ class PuzzleGame : AppCompatActivity() {
             croppedImageHeight
         )
 
-        // Calculate the with and height of the pieces
+       
         val pieceWidth = croppedImageWidth / cols
         val pieceHeight = croppedImageHeight / rows
 
-        // Create each bitmap piece and add it to the resulting array
+        
         var yCoord = 0
         for (row in 0 until rows) {
             var xCoord = 0
@@ -131,7 +128,7 @@ class PuzzleGame : AppCompatActivity() {
                     offsetY = pieceHeight / 3
                 }
 
-                // apply the offset to each piece
+                
                 val pieceBitmap = Bitmap.createBitmap(
                     croppedBitmap,
                     xCoord - offsetX,
@@ -146,20 +143,20 @@ class PuzzleGame : AppCompatActivity() {
                 piece.pieceWidth = pieceWidth + offsetX
                 piece.pieceHeight = pieceHeight + offsetY
 
-                // this bitmap will hold our final puzzle piece image
+                
                 val puzzlePiece = Bitmap.createBitmap(
                     pieceWidth + offsetX,
                     pieceHeight + offsetY,
                     Bitmap.Config.ARGB_8888
                 )
 
-                // draw path
+                
                 val bumpSize = pieceHeight / 4
                 val canvas = Canvas(puzzlePiece)
                 val path = Path()
                 path.moveTo(offsetX.toFloat(), offsetY.toFloat())
                 if (row == 0) {
-                    // top side piece
+                   
                     path.lineTo(pieceBitmap.width.toFloat(), offsetY.toFloat())
                 } else {
                     // top bump
@@ -178,7 +175,7 @@ class PuzzleGame : AppCompatActivity() {
                     path.lineTo(pieceBitmap.width.toFloat(), offsetY.toFloat())
                 }
                 if (col == cols - 1) {
-                    // right side piece
+                   
                     path.lineTo(pieceBitmap.width.toFloat(), pieceBitmap.height.toFloat())
                 } else {
                     // right bump
@@ -197,10 +194,10 @@ class PuzzleGame : AppCompatActivity() {
                     path.lineTo(pieceBitmap.width.toFloat(), pieceBitmap.height.toFloat())
                 }
                 if (row == rows - 1) {
-                    // bottom side piece
+                    
                     path.lineTo(offsetX.toFloat(), pieceBitmap.height.toFloat())
                 } else {
-                    // bottom bump
+                   
                     path.lineTo(
                         (offsetX + (pieceBitmap.width - offsetX) / 3 * 2).toFloat(),
                         pieceBitmap.height.toFloat()
@@ -216,7 +213,7 @@ class PuzzleGame : AppCompatActivity() {
                     path.lineTo(offsetX.toFloat(), pieceBitmap.height.toFloat())
                 }
                 if (col == 0) {
-                    // left side piece
+                    
                     path.close()
                 } else {
                     // left bump
@@ -235,7 +232,7 @@ class PuzzleGame : AppCompatActivity() {
                     path.close()
                 }
 
-                // mask the piece
+                
                 val paint = Paint()
                 paint.color = -0x1000000
                 paint.style = Paint.Style.FILL
@@ -243,21 +240,21 @@ class PuzzleGame : AppCompatActivity() {
                 paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
                 canvas.drawBitmap(pieceBitmap, 0f, 0f, paint)
 
-                // draw a white border
+               
                 var border = Paint()
                 border.color = -0x7f000001
                 border.style = Paint.Style.STROKE
                 border.strokeWidth = 8.0f
                 canvas.drawPath(path, border)
 
-                // draw a black border
+               
                 border = Paint()
                 border.color = -0x80000000
                 border.style = Paint.Style.STROKE
                 border.strokeWidth = 3.0f
                 canvas.drawPath(path, border)
 
-                // set the resulting bitmap to the piece
+               
                 piece.setImageBitmap(puzzlePiece)
                 pieces.add(piece)
                 xCoord += pieceWidth
@@ -271,28 +268,26 @@ class PuzzleGame : AppCompatActivity() {
         val ret = IntArray(4)
         if (imageView == null || imageView.drawable == null) return ret
 
-        // Get image dimensions
-        // Get image matrix values and place them in an array
+        
         val f = FloatArray(9)
         imageView.imageMatrix.getValues(f)
 
-        // Extract the scale values using the constants (if aspect ratio maintained, scaleX == scaleY)
+        
         val scaleX = f[Matrix.MSCALE_X]
         val scaleY = f[Matrix.MSCALE_Y]
 
-        // Get the drawable (could also get the bitmap behind the drawable and getWidth/getHeight)
+        
         val d = imageView.drawable
         val origW = d.intrinsicWidth
         val origH = d.intrinsicHeight
 
-        // Calculate the actual dimensions
+        
         val actW = Math.round(origW * scaleX)
         val actH = Math.round(origH * scaleY)
         ret[2] = actW
         ret[3] = actH
 
-        // Get image position
-        // We assume that the image is centered into ImageView
+        
         val imgViewW = imageView.width
         val imgViewH = imageView.height
         val top = (imgViewH - actH) / 2
@@ -334,28 +329,28 @@ class PuzzleGame : AppCompatActivity() {
         }
 
     private fun setPicFromPath(mCurrentPhotoPath: String, imageView: ImageView) {
-        // Get the dimensions of the View
+        
         val targetW = imageView.width
         val targetH = imageView.height
 
-        // Get the dimensions of the bitmap
+        
         val bmOptions = BitmapFactory.Options()
         bmOptions.inJustDecodeBounds = true
         BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions)
         val photoW = bmOptions.outWidth
         val photoH = bmOptions.outHeight
 
-        // Determine how much to scale down the image
+        
         val scaleFactor = Math.min(photoW / targetW, photoH / targetH)
 
-        // Decode the image file into a Bitmap sized to fill the View
+        
         bmOptions.inJustDecodeBounds = false
         bmOptions.inSampleSize = scaleFactor
         bmOptions.inPurgeable = true
         val bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions)
         var rotatedBitmap = bitmap
 
-        // rotate bitmap if needed
+       
         try {
             val ei = ExifInterface(mCurrentPhotoPath)
             val orientation = ei.getAttributeInt(
